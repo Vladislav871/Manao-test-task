6057ca893041bca2a06184c89223687b362a6355
Vladislav <vladislav.stelmakh02@gmail.com>
2020-12-25 11:11:46 +0300

    Validation

--- index.php
+++ index.php
@@ -13,31 +13,38 @@
         <div id="registration-decor">
             <h1>Registration</h1>
         </div>
-        <form class="registration-fields" action="#" method="post">
+        <form id="registration" class="registration-fields" action="#" method="post">
             <div class="field">
                 <p>Login(unique)</p>
-                <input type="text" required>
+                <input name="login" type="text">
+                <div id="loginMessage"><span></span></div>
             </div>
             <div class="field">
                 <p>Password</p>
-                <input type="text" required>
+                <input name="pass" type="text">
+                <div id="passMessage"><span></span></div>
             </div>
             <div class="field">
                 <p>Confirm password</p>
-                <input type="text" required>
+                <input name="confirm_pass" type="text">
+                <div id="confirmMessage"><span></span></div>
             </div>
             <div class="field">
                 <p>E-mail(unique)</p>
-                <input type="email" required>
+                <input name="email" type="email">
+                <div id="emailMessage"><span></span></div>
             </div>
             <div class="field">
                 <p>Name</p>
-                <input type="text" required>
+                <input name="name" type="text">
+                <div id="nameMessage"><span></span></div>
             </div>
-            <p>If you already have an account <a href="login_form/login.html">login in</a></p>
+            <p>If you already have an account <a href="login_form/login.php">login in</a></p>
             <button type="submit"><p>Create</p></button>
         </form>
     </div>
 </section>
+<script src="scripts/jquery-3.5.1.min.js"></script>
+<script src="scripts/query.js"></script>
 </body>
 </html>
